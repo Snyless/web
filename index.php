036f553097a56360f1d1b4671d189688cd50ec7f
oumaimaZ <oumaimazr2@gmail.com>
2017-05-15 03:47:00 +0100

    sjsjs

--- index.php
+++ index.php
@@ -9,24 +9,15 @@
 				<a href="single.php"><img src="images/ba.png" alt="" class="img-responsive"></a>
 			</div>
 			<div class="col-md-6 banner-right simpleCart_shelfItem">
-				<span class="bann-heart"> </span>
-				<h2>Handbags</h2>
-				<h1>Sed perspiciatis unde</h1>
-				<h5 class="item_price">$200.00</h5>
-				<ul class="bann-small-img">
-					<li><a href="single.php"><img src="images/b1.jpg"></a></li>
-					<li><a href="single.php"><img src="images/b2.jpg"></a></li>
-				</ul>
-				<h6>Size Charts</h6>
+				<span ></span>
+				<h2>Bienvenue</h2>
+				<h1>Shop by Oumaima</h1>
+				
+				
+				
 				<ul class="bann-btns">
-				<li><select class="bann-size">
-					<option value="select your location">Size</option>
-					<option value="saab">Small</option>
-					<option value="fiat">Medium</option>
-					<option value="audi">Large</option>
-				</select>
-				</li>
-				<li><a href="#" class="item_add" >Add To Cart</a></li>
+				
+				
                </ul>
 			</div>
 			<div class="clearfix"> </div>
--- product-m.php
+++ product-m.php
@@ -9,28 +9,56 @@
 					
 				</div>
 			  </div>	
+			 <?php 
+					    try
+						    {
+							     $bd=new PDO('mysql:host=localhost;dbname=data_projet;charset=utf8','root','',
+	                             array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+						    }
+			  
+                          catch(Exception $e)
+						    {
+	                             die('Erreur : '.$e->getMessage());
+		                    }
+							
+						
+	$req1=$bd->query('select pr.* from produit pr
+		');
+						while($data1=$req1->fetch())
+						{   if(isset($_POST[$data1['ID']])){
+								$d=$bd->quezy'Insert into panier values() ');
+								
+								break;
+							}
+						}
+						
+						
+						
+				
+$reponse=$req1=$bd->query('select pr.* from produit pr');
+		                  $reponse->execute();
+                
+						  while($req=$reponse->fetch()){
+                              ?>
+	
 			  <div class="col-md-12 product-block">
 			      <div class="col-md-4 home-grid">
 					<div class="home-product-main">
 					   <div class="home-product-top">
-					      <a href="single.php"><img src="images/h19.jpg" alt="" class="img-responsive zoom-img"></a>
+					      <a href="#"><img src="images/h19.jpg" alt="" class=" zoom-img"></a>
 					   </div>
-						<div class="home-product-bottom">
-								<h3><a href="single.php">Smart Shopping</a></h3>
-								<p>Explore Now</p>						
+					   <h3> <form method="post" action="produit-m.php">
+					   	<button class="btn btn-sm btn-warning pull-right" type="submit" name="<?php echo $req['ID'];?>" >add</button></h3>
+						</form><div class="home-product-bottom">
+		
+								<p> <?php echo $req['nom'];?></p>						
 						</div>
 						<div class="srch">
-							<span>$200</span>
+							<span><?php echo $req['prix'];?></span>
 						</div>
 					</div>
 				 </div>
-			      
-			     
-			   
-			      
-			      
-			    
-			      
+			   <?php } ?>
 			      
 			      <div class="clearfix"> </div>
 			  </div>
